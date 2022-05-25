c6a8c4693c9bfba4c98a6b045dbd61ec8adfc7f2
Ado Barman <adobarman3@gmail.com>
2022-05-25 07:15:30 +0530

    Updates

---
+++ precache-manifest.fa72137d3cf9a7c5f0ac9a6365ee90fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cf6bb656875585074ea447080f8353",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/note-keeper/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "f771421ace7a7f0cc597",
+    "url": "/note-keeper/static/js/main.621cb946.chunk.js"
+  },
+  {
+    "revision": "62701cc2e54e0655e214",
+    "url": "/note-keeper/static/js/runtime-main.216d5a5d.js"
+  }
+]);
